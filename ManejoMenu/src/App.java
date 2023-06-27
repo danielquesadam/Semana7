04138f1395470754e4dd727aebe1f28308f30b16
danielquesadam <danquesamo@hotmail.com>
2023-06-26 20:05:53 -0600

    Cambios1
    App.java
    Menu.java

---
+++ ManejoMenu/src/App.java
@@ -0,0 +1,10 @@
+import BussinessLogic.Menu;
+
+//Que es la clase APP es la que inicia nuestra aplicacion
+public class App {
+public static void main(String[] args) {
+    //Paso 1. Vamos a definir la clase manejadora del sistema para que sea la primera en ejecutarse */
+    //Para esto creamos una instanci de la clase Menu
+new Menu();
+}
+}
---
+++ ManejoMenu/src/BussinessLogic/Menu.java
@@ -0,0 +1,12 @@
+package BussinessLogic;
+//Es la clase encargada de manejar el menu y las diferentes opciones o operaciones del sistema.
+//Es decir es la clase orquestadora del sistema
+public class Menu {
+//Constructor de la clase
+//El constructor siempre se llama igual que la clase
+//Y definir como va a iniciar la clase
+//Es el primer metodo que se ejecuta cuando se crea una instancia de la clase
+public Menu(){
+    System.out.println("Hola estoy en Menu");
+}
+}
