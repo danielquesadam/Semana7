2a8f70aad062eaac7c3e7ba9def1efdd7a11b17e
danielquesadam <danquesamo@hotmail.com>
2023-06-27 23:54:24 -0600

    Update App.java

--- ManejoMenu/src/App.java
+++ ManejoMenu/src/App.java
@@ -2,9 +2,10 @@ import BussinessLogic.Menu;
 
 //Que es la clase APP es la que inicia nuestra aplicacion
 public class App {
-public static void main(String[] args) {
-    //Paso 1. Vamos a definir la clase manejadora del sistema para que sea la primera en ejecutarse */
-    //Para esto creamos una instanci de la clase Menu
-new Menu();
-}
+    public static void main(String[] args) {
+        // Paso 1. Vamos a definir la clase manejadora del sistema para que sea la
+        // primera en ejecutarse */
+        // Para esto creamos una instanci de la clase Menu
+        new Menu();
+    }
 }
